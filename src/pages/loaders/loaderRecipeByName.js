1474b7f4ad978a5f982ff5619734c84e3068c4b4
victor <victorgomezferrer@gmail.com>
2024-03-23 20:56:26 +0100

    1

--- src/pages/loaders/loaderRecipeByName.js
+++
@@ -1,8 +0,0 @@
-import recipesService from "../../services/recipes.service"
-
-const loaderRecipeByName = async (name) => {
-    const recipes = await recipesService.getRecipeByName(name)
-    return recipes
-}
-
-export default loaderRecipeByName
