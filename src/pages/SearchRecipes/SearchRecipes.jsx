ec74d0394db5dd3e7b4ff1a84fab695b80c9ddf3
victor <victorgomezferrer@gmail.com>
2024-03-23 20:59:03 +0100

    111111

--- src/pages/SearchRecipes/SearchRecipes.jsx
+++ src/pages/SearchRecipes/SearchRecipes.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
+import { Button, Input } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import recipesService from '../../services/recipes.service'
+import RecipesCards from '../../components/RecipesCards/RecipesCards'
+
 
 const SearchRecipes = () => {
+
+    let [inputValues, setImputValues] = useState()
+    let [searchResults, setSearchResults] = useState([])
+
+    const onChange = (e) => {
+        const searchInput = e.target.value
+        setImputValues(searchInput)
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            const response = await recipesService.getRecipeByName(inputValues)
+            console.log(response)
+            setSearchResults(response)
+
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    console.log(searchResults)
     return (
         <>
             <h1>hola</h1>
+            <form onSubmit={onSubmit}>
+                <Input onChange={onChange} name={'searchInput'} ></Input>
+                <Button type='submit'>eeeeee</Button>
+            </form>
+
+
         </>
     )
 }
-
-export default SearchRecipes
